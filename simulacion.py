d4867f8294a968c61799b51fa892777caacc009b
sanloso <sanloso25@hotmail.com>
2020-03-09 21:50:05 -0500

    simulacion

---
+++ simulacion.py
@@ -0,0 +1,30 @@
+from calculadoraCom import *
+
+def main():
+   print()
+   
+def canicas(proba,vector,rendijas):
+   for i in range(rendijas):
+      vector = multi_matrices(proba,vector)
+   return vector
+
+def  cuantico_de_rendijas(proba,vector,rendijas,target):
+   if len(proba) != target:
+      print("Hay un error entre el taget y la matriz")
+   else:
+      for i in range(rendijas):
+         vector = multi_matrices(proba,vector)
+   return vector
+
+def probabilistico_mas_de_dos_rendijas(proba,vector,rendijas,target):
+   if len(proba) != target:
+      print("Hay un error entre el taget y la matriz")
+   else:
+      for i in range(rendijas):
+         vector = multi_matrices(proba,vector)
+   return vector
+
+
+
+   
+main()
